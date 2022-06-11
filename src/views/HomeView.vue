4b79a6617fe66266599429482561c67d4cab57ba
Quintin <zelotbe@gmail.com>
2022-06-11 14:20:42 +0200

    Fixed 'global' error

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from "@/components/TheWelcome.vue";
+import Solid from "@/components/Solid.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Solid />
   </main>
 </template>
