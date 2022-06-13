c81c1b39b387a2135f5190e2712d9faacf892bc5
Quintin <zelotbe@gmail.com>
2022-06-13 02:51:51 +0200

    Moved logout to auth store

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,3 +1,6 @@
+import { logout } from "@inrupt/solid-client-authn-browser";
+import router from "../router";
+
 export default {
   namespaced: true,
   state() {
@@ -13,6 +16,9 @@ export default {
     },
     SET_WEBID(state, value) {
       state.webID = value;
+    },
+    SET_ERROR(state, value) {
+      state.error = value;
     },
   },
   getters: {
@@ -32,6 +38,15 @@ export default {
     },
     setWebID({ commit }, url) {
       commit("SET_WEBID", url);
+    },
+    setError({ commit }, error) {
+      commit("SET_ERROR", error);
+    },
+    logoutUser({ commit }) {
+      logout();
+      window.localStorage.clear();
+      router.push("/");
+      commit("SET_LOGGEDIN", false);
     },
   },
 };
